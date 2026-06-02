0ba25de8bbd273c3bec1c244641821348d8a0da2
Seth Diaz <adiazmonta@gmail.com>
2026-06-02 17:28:00 -0600

    No se que estoy heciendo

--- frontend/js/login.js
+++ frontend/js/login.js
@@ -1,31 +1,42 @@
-document.addEventListener("DOMContentLoaded", () => {
+const API_URL = "https://api-tlapiani-1.onrender.com/api/auth/login";
 
+document.addEventListener("DOMContentLoaded", () => {
     const form = document.getElementById("loginForm");
     const mensaje = document.getElementById("mensajeError");
 
-    form.addEventListener("submit", async (e) => {
-
+    form.addEventListener("submit", async function (e) {
         e.preventDefault();
 
-        const usuario = document.getElementById("usuario").value.trim();
+        const usuarioOCorreo = document.getElementById("usuario").value.trim();
         const password = document.getElementById("password").value.trim();
-
-        if (!usuario || !password) {
-
-            mensaje.textContent =
-                "Por favor ingresa tu usuario o correo y contraseña.";
-
-            return;
-        }
 
         mensaje.textContent = "";
 
-        console.log({
-            usuario,
-            password
+        if (!usuarioOCorreo || !password) {
+            mensaje.textContent = "Por favor ingresa tu usuario o correo y contraseña.";
+            return;
+        }
+
+        const respuesta = await fetch(API_URL, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                usuarioOCorreo,
+                password
+            })
         });
 
-        // Aquí después haremos el fetch al backend
-    });
+        const data = await respuesta.json();
 
+        if (!respuesta.ok) {
+            mensaje.textContent = data.mensaje || "No se pudo iniciar sesión";
+            return;
+        }
+
+        localStorage.setItem("token", data.token);
+
+        window.location.href = "dashboard.html";
+    });
 });
